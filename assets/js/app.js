55d8b1b985ee4930744493061707a68fb2af132c
QusaiIsmail97 <142750490+QusaiIsmail97@users.noreply.github.com>
2024-01-15 09:54:01 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './theme';
-import { GlobalStyles } from './global';
+import './styles/App.css';
+import React, { useEffect, useState } from 'react';
+import NavBar from './components/NavBar';
+import Blog from './components/Blog';
+import Speaking from './components/Speaking';
+import Work from './components/Work';
+import About from './components/About';
+import Contact from './components/Contact';
+import { keepTheme } from './utils/themes';
+import { useScrollToTop } from './utils/scrollToTop';
 
 function App() {
+  const [className, setClassName] = useState("theme-dark");
+
+  useScrollToTop();
+  useEffect(() => {
+    keepTheme(setClassName)
+  }, [setClassName])
+
   return (
-    <ThemeProvider theme={lightTheme}>
-      <>
-        <GlobalStyles />
-        <button>Toggle theme</button>
-        <h1>It's a light theme!</h1>
-        <footer>
-        </footer>
-      </>
-    </ThemeProvider>
+    <div className={`App ${className}`}>
+      <div className="main-container">
+        <h1 className="offscreen">Abbey Perini's Portfolio</h1>
+        <NavBar setClassName={setClassName}/>
+        <Blog home={true} />
+        <Speaking home={true} />
+        <Work home={true} />
+        <About home={true} />
+        <Contact home={true} />
+      </div>
+    </div>
   );
 }
 
